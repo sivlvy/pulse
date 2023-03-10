bd67ac575064ae684afb013a5d5fc1067b780c61
sivlvvy <vladislavsidenko@ukr.net>
2023-03-10 15:42:57 +0200

    4 commit

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
       prevArrow: '<button type="button" class="slick-prev"><img src="../img/solid/arrow_left.svg"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="../img/solid/arrow_right.svg"></button>',
       responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: false,
+            dots: true
+            }
+         },
             {
                breakpoint: 992,
                settings: {
@@ -15,9 +24,25 @@ $(document).ready(function(){
                {
                   breakpoint: 575,
                   settings: {
-                     dots: false,
+                     dots: true,
                      slidesToShow: 1,
                      slidesToScroll: 1
+                  }
+               },
+               {
+                  breakpoint: 425,
+                  settings: {
+                     dots:true,
+                     slidesToShow: 1,
+                     slidesToScroll: 1,
+                     arrows: false
+                  }
+               },
+               {
+                  breakpoint: 375,
+                  settings: {
+                     dots:true,
+
                   }
                }
       ]
@@ -43,4 +68,22 @@ $(document).ready(function(){
 
    toggleSlide('.catalog-item__link');
    toggleSlide('.catalog-item__back');
+
+   //Modal//
+
+   $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn();
+   });
+   $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #thanks, #order').fadeOut('fast')
+   });
+   $('.button_mini').on('click', function() {
+      $('.overlay, #order').fadeIn();
+   });
+
+   $('#consultation-form').validate();
+   $('#consultation form').validate({
+      
+   });
+   $('#order form').validate();
 });
