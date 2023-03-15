00dd81f0a13d2e506e88e630df809a04fa112743
sivlvvy <vladislavsidenko@ukr.net>
2023-03-15 15:57:07 +0200

    6 commit

--- src/js/script.js
+++ src/js/script.js
@@ -81,9 +81,57 @@ $(document).ready(function(){
       $('.overlay, #order').fadeIn();
    });
 
-   $('#consultation-form').validate();
-   $('#consultation form').validate({
-      
+   function validateForms(form){
+      $(form).validate({
+         rules: {
+            name: {
+                  required: true,
+                  minlength: 2
+            },
+            phone: "required",
+            email: {
+                  required: true,
+                  email: true
+            }
+         },
+         messages: {
+            name: {
+               required: "Пожалуйста, введите свое имя",
+                  minlength: jQuery.validator.format("Введите {0} символа!")
+               },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+               required: "Пожалуйста, введите свою почту",
+               email: "Неправильно введен адрес почты"
+            }
+         }
+      });
+};
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form')
+
+$('input[name=phone]').mask("+38 (999) 999-99-99");
+
+$('form').sumbit(function(e) {
+      e.preventDefault();
+
+      if (!$(this).valid()) {
+         return;
+      }
+
+
+      $.ajax({
+         type: "POST",
+         url: "mailer/smart.php",
+         data: $(this).serialize()
+      }).done(function() {
+         $(this).find("input").val("");
+         $('#consultation, #order').fadeOut();
+         $('.overlay, #thanks').fadeIn('slow');
+         
+         $('form').trigger('reset');
+      });
+      return false;
    });
-   $('#order form').validate();
 });
