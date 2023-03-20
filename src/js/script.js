4636e52d25c9a49091c65e2da67a9368aa94a00c
sivlvvy <vladislavsidenko@ukr.net>
2023-03-20 16:04:08 +0200

    8 commit

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
    $('.carousel__inner').slick({
       speed: 1000,
       // adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="../img/solid/arrow_left.svg"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="../img/solid/arrow_right.svg"></button>',
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/solid/arrow_left.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/solid/arrow_right.svg"></button>',
       responsive: [
          {
             breakpoint: 1024,
